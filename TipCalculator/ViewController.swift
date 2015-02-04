59357ecde176bdc73e851c7a35b29e0fc766d50b
张玉泉 <1289194527@qq.com>
2015-02-04 08:10:16 +0800

    TipCalculator--小费计算器
    TipCalculator--小费计算器
    Swift写得一个小费计算器的Demo

--- TipCalculator/ViewController.swift
+++ TipCalculator/ViewController.swift
@@ -10,6 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var tipRateSegmentedControl: UISegmentedControl!
+    @IBOutlet weak var billTextField: UITextField!
+    
+    @IBAction func calculateTapped(sender: AnyObject) {
+        
+        //1. Get the total bill
+        var userInput = billTextField.text as NSString
+        var totalBill: Float = userInput.floatValue
+        //2. Determine the tip rate
+        var index: Int = tipRateSegmentedControl.selectedSegmentIndex
+        var tipRate: Float = 0.15
+        if index == 0{
+            tipRate = 0.15
+        }else if index == 1{
+            tipRate = 0.20
+        }else{
+            tipRate = 0.25
+        }
+        //3. Calculate the Tip
+        var tip : Float = totalBill * tipRate
+        //4. Display the Tip
+        tipLabel.text = "$\(tip)"
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
